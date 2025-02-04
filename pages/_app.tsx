cc80c2b4d0f2ea4a0deddbe8fb5e24aa8d5c37c6
asdfstudio <sss.joy.one@gmail.com>
2025-02-04 19:42:52 +0600

    bal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       {/* <QueryClientProvider client={queryClient}> */}
       <Provider store={store}>
         {/* <ReactQueryDevtools /> */}
-        <NavigationMenuDemo />
+        {/* <NavigationMenuDemo /> */}
         <Component {...pageProps} />
       </Provider>
       {/* </QueryClientProvider> */}
