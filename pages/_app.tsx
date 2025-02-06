862f7320f0a323c84a23ee750be93663a5ea240b
asdfstudio <sss.joy.one@gmail.com>
2025-02-06 17:40:24 +0600

    test1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,21 @@ import type { AppProps } from "next/app";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import store from "@/store";
+import Head from "next/head";
 
 // const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="screen-orientation" content="portrait" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black"
+        ></meta>
+      </Head>
       {/* <QueryClientProvider client={queryClient}> */}
       <Provider store={store}>
         {/* <ReactQueryDevtools /> */}
